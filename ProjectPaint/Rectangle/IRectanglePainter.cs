b482ddc343dd72e6faa2d7a5f44e7abe15797f36
luong07734 <le.ky.luong@gmail.com>
2022-04-25 09:39:14 +0700

    [19127208] Draw in all angel

--- ProjectPaint/Rectangle/IRectanglePainter.cs
+++ ProjectPaint/Rectangle/IRectanglePainter.cs
@@ -16,21 +16,55 @@ namespace RectangleEntity
         public UIElement Draw(IShapeEntity shape)
         {
             var rectangle = shape as RectangleEntity;
-
-            // TODO: chú ý việc đảo lại rightbottom và topleft 
-            double width = rectangle.RightBottom.X - rectangle.TopLeft.X;
-            double height = rectangle.RightBottom.Y - rectangle.TopLeft.Y;
-
-            var element = new Rectangle()
+            double width = 0;
+            double height = 0;
+            Rectangle element = null;
+            element = new Rectangle()
             {
                 Width = width,
                 Height = height,
                 StrokeThickness = 1,
                 Stroke = new SolidColorBrush(Colors.Red)
             };
-            Canvas.SetLeft(element, rectangle.TopLeft.X);
-            Canvas.SetTop(element, rectangle.TopLeft.Y);
 
+            if (rectangle.RightBottom.X - rectangle.TopLeft.X >= 0 && rectangle.RightBottom.Y - rectangle.TopLeft.Y >= 0)
+            {
+                width = rectangle.RightBottom.X - rectangle.TopLeft.X;
+                height = rectangle.RightBottom.Y - rectangle.TopLeft.Y;
+                element.Width = width;
+                element.Height = height;
+                Canvas.SetLeft(element, rectangle.TopLeft.X);
+                Canvas.SetTop(element, rectangle.TopLeft.Y);
+            }
+            else if (rectangle.RightBottom.X - rectangle.TopLeft.X <= 0 && rectangle.RightBottom.Y - rectangle.TopLeft.Y >= 0)
+            {
+                width = -rectangle.RightBottom.X + rectangle.TopLeft.X;
+                height = rectangle.RightBottom.Y - rectangle.TopLeft.Y;
+                element.Width = width;
+                element.Height = height;
+                Canvas.SetLeft(element, rectangle.RightBottom.X);
+                Canvas.SetTop(element, rectangle.TopLeft.Y);
+            }
+
+            else if (rectangle.RightBottom.X - rectangle.TopLeft.X <= 0 && rectangle.RightBottom.Y - rectangle.TopLeft.Y <= 0)
+            {
+                width = -rectangle.RightBottom.X + rectangle.TopLeft.X;
+                height = -rectangle.RightBottom.Y + rectangle.TopLeft.Y;
+                element.Width = width;
+                element.Height = height;
+                Canvas.SetLeft(element, rectangle.RightBottom.X);
+                Canvas.SetTop(element, rectangle.RightBottom.Y);
+            }
+            else
+            {
+                width = rectangle.RightBottom.X - rectangle.TopLeft.X;
+                height = -rectangle.RightBottom.Y + rectangle.TopLeft.Y;
+                element.Width = width;
+                element.Height = height;
+                Canvas.SetLeft(element, rectangle.TopLeft.X);
+                Canvas.SetTop(element, rectangle.RightBottom.Y);
+
+            }
             return element;
         }
     }
