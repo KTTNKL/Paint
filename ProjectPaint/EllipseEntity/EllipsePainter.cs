5e3cb1288c468c8d4e54f0289e43c9f767cf9d78
19127312 <19127312@student.hcmus.edu.vn>
2022-04-25 08:59:16 +0700

    Init

---
+++ ProjectPaint/EllipseEntity/EllipsePainter.cs
@@ -0,0 +1,39 @@
+﻿using IContract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Shapes;
+
+namespace EllipseEntity
+{
+    class EllipsePainter : IPaintBusiness
+    {
+        public UIElement Draw(IShapeEntity shape)
+        {
+            var ellipse = shape as EllipseEntity;
+
+            // TODO: chú ý việc đảo lại rightbottom và topleft 
+            double width = ellipse.RightBottom.X - ellipse.TopLeft.X;
+            double height = ellipse.RightBottom.Y - ellipse.TopLeft.Y;
+
+            var element = new Ellipse()
+            {
+                Width = width,
+                Height = height,
+                StrokeThickness = 1,
+                Stroke = new SolidColorBrush(Colors.Red)
+            };
+            Canvas.SetLeft(element, ellipse.TopLeft.X);
+            Canvas.SetTop(element, ellipse.TopLeft.Y);
+
+            return element;
+        }
+       
+    }
+
+}
